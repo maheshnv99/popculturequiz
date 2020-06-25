8cc89ce649c909e50583155bb5275a0e0b802cae
maheshnv99 <mahesh.nandini66@gmail.com>
2020-06-25 20:36:58 +0530

    Updates

---
+++ precache-manifest.4239334b16374c5eafc44f1972ef3a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a770e9d5245ba3eccd2ca55cf9e409",
+    "url": "/popculturequiz/index.html"
+  },
+  {
+    "revision": "0382e404fbdc89fdc030",
+    "url": "/popculturequiz/static/css/main.4ad739f5.chunk.css"
+  },
+  {
+    "revision": "38707783e4090537211d",
+    "url": "/popculturequiz/static/js/2.edf4f649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/popculturequiz/static/js/2.edf4f649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0382e404fbdc89fdc030",
+    "url": "/popculturequiz/static/js/main.7a8630a6.chunk.js"
+  },
+  {
+    "revision": "a9a7bbbe65d25bceb30c",
+    "url": "/popculturequiz/static/js/runtime-main.3d8a1a0a.js"
+  }
+]);
